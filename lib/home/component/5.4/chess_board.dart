2e107bb568a4f5260aa5fe0d2d948034d7b02ca8
DELL <sharmashalu3148@gmail.com>
2024-04-11 07:54:53 +0530

    11-04-24

---
+++ lib/home/component/5.4/chess_board.dart
@@ -0,0 +1,70 @@
+import 'package:e_commerce/home/component/5.4/chess_box.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp(),);
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => HomeState();
+}
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        shadowColor: Colors.black,
+        elevation: 5,
+        title: const Text(
+          'Chess Board',style: TextStyle(
+          color: Colors.white,fontSize: 20,
+        ),
+        ),
+      ),
+      body: Align(
+        child: Container(
+          height: 442,
+          width:442,
+          alignment: Alignment.topLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.black,
+              )
+          ),
+
+          child: Column(
+            children: [
+              Box2(),
+              Box1(),
+              Box2(),
+              Box1(),
+              Box2(),
+              Box1(),
+              Box2(),
+              Box1(),
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/home/component/5.4/chess_box.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+// Container (
+// height: 55,
+// width:55,
+// decoration: const BoxDecoration(
+// color: Colors.black,
+// ),
+// ),
+// Container (
+// height: 55,
+// width: 55,
+// decoration:const BoxDecoration(
+// color: Colors.white,
+// ),
+// )
+Row Box2() {
+  return Row(
+    children: [
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(color: Colors.white),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        color: Colors.black,
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(color: Colors.white),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        color: Colors.black,
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(color: Colors.white),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+      ),
+    ],
+  );
+}
+
+Row Box1() {
+  return Row(
+    children: [
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(color: Colors.black),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        color: Colors.white,
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        color: Colors.white,
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        color: Colors.white,
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+      ),
+      Container(
+        height: 55,
+        width: 55,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+      ),
+    ],
+  );
+}
