de4e100475cfa5ebf63568010143ab4e1078dcd6
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-04-11 13:39:19 +0530

    Icon_udf

--- lib/home/component/5.4/Icon_udf.dart
+++ lib/home/component/5.4/Icon_udf.dart
@@ -1,22 +1,23 @@
+
 import 'package:flutter/Material.dart';
 import 'package:flutter/cupertino.dart';
 
 Widget IconBox({required IconData icon}) {
   return Container(
-    height: 100,
-    width: 100,
-    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+    height: 120,
+    width: 120,
+    margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
     decoration:  BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      color: Color(0xfff5f5f5),
+        borderRadius: BorderRadius.circular(20),
+        color: Color(0xfff5f5f5),
         boxShadow: [
-      BoxShadow(
-        offset: Offset(0, 8),
-        color: Color(0xFFBABABA),
-        spreadRadius: 3,
-        blurRadius: 8,
-      )
-    ]),
+          BoxShadow(
+            offset: Offset(0, 8),
+            color: Color(0xFFBABABA),
+            spreadRadius: 3,
+            blurRadius: 8,
+          )
+        ]),
     child: Icon(
       icon,
       size: 30,
